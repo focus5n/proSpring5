66eb4bacd989760dc266782fc5ad76d589fe6e7b
hans <focus5n@naver.com>
2023-12-06 16:10:21 +0900

    Initial Commit

---
+++ src/main/java/HelloProSpring5.java
@@ -0,0 +1,8 @@
+public class HelloProSpring5 {
+
+    public static void main(String... args) {
+        System.out.println(":::::::::::::::::: RUN MAIN ::::::::::::::::::");
+        System.out.println("Hello, ProSpring5!");
+        System.out.println(":::::::::::::::::: END MAIN ::::::::::::::::::");
+    }
+}
